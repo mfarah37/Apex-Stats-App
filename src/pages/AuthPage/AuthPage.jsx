2bc941fdc9526cbb8e3cfaffb9ede43e44c1bebd
Mohamed <mfarah37@gmail.com>
2022-02-14 01:30:05 -0800

    removes comments

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,7 +2,7 @@ import LoginForm from "../../components/LoginForm/LoginForm"
 import SignUpForm from "../../components/SignUpForm/SignUpForm"
 
 export default function AuthPage({ setUser }) {
-    return(
+    return (
         <main className='container'>
             <h1>Apex Stats</h1>
             <SignUpForm setUser = {setUser} />
